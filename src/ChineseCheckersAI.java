46fa33aa0f06f74767a35a15506321e4a3e1a098
Feng Guo <feng.guo2001@gmail.com>
2019-04-24 22:57:33 -0400

    i thought gitignore ignored the fucking class files tf

--- src/ChineseCheckersAI.java
+++ src/ChineseCheckersAI.java
@@ -9,35 +9,107 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+
+import java.net.UnknownHostException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collection; //Why do we need this
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
+//import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 //main class for the server
 public class ChineseCheckersAI {
-	
-	//stuff we dont need
-	private JButton sendButton, clearButton;
-	  private JTextField typeField;
-	  private JTextArea msgArea;  
-	  private JPanel southPanel;
-	  private Socket mySocket; //socket for connection
-	  private BufferedReader input; //reader for network stream
-	  private PrintWriter output;  //printwriter for network output
-	  private boolean running = true; //thread status via boolean
-	
-	public static void main(String [] args) {
-		//start the boie
-	}
-	
-	ChineseCheckersAI(){
-		
-	}
-	
+  
+  //stuff we dont need
+//  private JButton sendButton, clearButton;
+//  private JTextField typeField;
+//  private JTextArea msgArea;  
+//  private JPanel southPanel;
+  
+  
+  //GUI 
+  JFrame mainFrame;
+  JPanel startPanel;
+  
+  //Server 
+  private Socket mySocket; //socket for connection
+  private BufferedReader input; //reader for network stream
+  private PrintWriter output;  //printwriter for network output
+  
+  
+  private boolean running = true; //thread status via boolean
+  
+  public static void main(String [] args) {
+    ChineseCheckersAI chineseCheckersAI = new ChineseCheckersAI();
+  }
+  
+  ChineseCheckersAI(){
+    setUp();
+  }
+  
+  private void connectToServer(String ip, int port) {
+    try {
+      mySocket = new Socket(ip, port);
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  private void setUp(){
+    mainFrame = new JFrame("Chinese Checkers AI");
+    mainFrame.setVisible(true);
+    mainFrame.setSize(400,400);
+    
+    startPanel = new JPanel();
+    startPanel.setVisible(true);
+    JLabel serverIPLabel = new JLabel("Enter the IP Address");
+    JTextField serverIPTextField = new JTextField(20);
+    JLabel portLabel = new JLabel("Enter the port");
+    JTextField portTextField = new JTextField(10);
+    JButton okayButton = new JButton("Okay");
+    okayButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent actionEvent) {
+        try {
+          String ip = serverIPTextField.getText();
+          int port = Integer.parseInt(portTextField.getText());
+          if (ip != null && !"".equals(ip)) {
+            connectToServer(ip, port);
+          } else {
+            System.out.println("IP Address is blank!");
+          }
+        } catch (NumberFormatException e) {
+          System.out.println("Not a valid port!");
+          e.printStackTrace();
+        } 
+      }
+    });
+    
+    startPanel.add(serverIPLabel);
+    startPanel.add(serverIPTextField);
+    startPanel.add(portLabel);
+    startPanel.add(portTextField);
+    startPanel.add(okayButton);
+    mainFrame.add(startPanel);
+    repaintFrame();
+  }
+  
+  private void repaintFrame() {
+    mainFrame.setVisible(true);
+    mainFrame.repaint();
+  }
 }
---
+++ src/ChineseCheckersAI.java~
@@ -0,0 +1,43 @@
+/* [ChineseCheckersAI.java]
+ * Client class that connects to a server to act as a bot playing chinese checkers
+ * @author Bill Liu, Feng Guo, Victor Lin
+ * April 23, 2019
+ */
+
+//imports
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+//main class for the server
+public class ChineseCheckersAI {
+	
+	//stuff we dont need
+	private JButton sendButton, clearButton;
+	  private JTextField typeField;
+	  private JTextArea msgArea;  
+	  private JPanel southPanel;
+	  private Socket mySocket; //socket for connection
+	  private BufferedReader input; //reader for network stream
+	  private PrintWriter output;  //printwriter for network output
+	  private boolean running = true; //thread status via boolean
+	
+	public static void main(String [] args) {
+		//start the boie
+	}
+	
+	ChineseCheckersAI(){
+		
+	}
+	
+}
